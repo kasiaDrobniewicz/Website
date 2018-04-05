437f8227cdcb25492587fdda4402cfe7e98b92a1
Kasia Drobniewicz <kasia.drobniewicz@gmail.com>
2018-04-05 14:27:21 +0200

    Add script file

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function myTeam() {
+  document.getElementById("demo").innerHTML = "Melon to grupa profesjonalistów, która efektywnie zaplanuje, przygotuje i zorganizuje dla Państwa każdy rodzaj eventu. Doświadczenie zdobyte podczas organizacji wielu imprez, pozwala nam oferować profesjonalną obsługę na najwyższym poziomie. Zapewnimy kompleksową organizację zarówno konferencji, kongresów, seminariów, szkoleń, jak i imprez integracyjnych, spotkań biznesowych, targów czy też akcji promocyjnych. Gwarantujemy indywidualne podejście do Klienta, najwyższy poziom oferowanych przez nas usług, a także sprawność w zakresie działań organizacyjnych. Nasze usługi świadczymy kompleksowo lub tylko w wybranym zakresie, na terenie całej Polski. Organizujemy eventy zarówno dla grup od kilkudziesięciu do kilkuset uczestników.";
+}
+
+function myOfert() {
+  document.getElementById("demo1").innerHTML = "Melon to grupa profesjonalistów, która efektywnie zaplanuje, przygotuje i zorganizuje dla Paostwa każdy rodzaj przedsięwzięcia. Doświadczenie zdobyte podczas organizacji wielu imprez, pozwala nam oferowad profesjonalną obsługę na najwyższym poziomie. Zapewnimy kompleksową organizację zarówno kongresów, seminariów, konferencji, szkoleo, jak i imprez integracyjnych, spotkao biznesowych, targów czy też akcji promocyjnych. Gwarantujemy indywidualne podejście do Klienta, najwyższy poziom oferowanych przez nas usług, a także sprawnośd w zakresie działao organizacyjnych. Nasze usługi świadczymy kompleksowo lub tylko w wybranym zakresie, na terenie całej Polski. Organizujemy eventy zarówno dla grup od kilkudziesięciu do kilkuset uczestników";
+}
+
+function myFunction() {
+  document.getElementById("demo1").innerHTML = "Konkretna, szczegółowo dba o najmniejsze detale organizowanych eventów. Doświadczenie zdobywała, pracując w prestiżowych krakowskich agencjach public relations, zajmując się organizacją dużych eventów oraz imprez firmowych.";
+}
